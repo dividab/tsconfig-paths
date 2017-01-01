d2cdc67bff21b9c6a61e627ac2bff6f6f33f656a
jonathanmourtada <jonathan.mourtada@divid.se>
2017-01-01 19:38:58 +0100

    refactor package-reader

--- src/package-reader.ts
+++ src/package-reader.ts
@@ -1,14 +1,19 @@
 import * as fs from "fs";
-
+/**
+ * @param  packageJsonPath Path to package.json
+ * @param  loadPackageJson Function that reads and parses package.json.
+ * @param  fileExists Function that checks for existance of a file.
+ * @returns string
+ */
 export function readPackage(
   packageJsonPath: string,
-  loadPackageJson: (file: string) => any = loadPackageJsonFromDisk,
+  loadPackageJson: (file: string) => any = loadJsonFromDisk,
   fileExists: (path: string) => boolean = fs.existsSync
 ): { main: string } | undefined {
 return (packageJsonPath.match(/package\.json$/) && fileExists(packageJsonPath) && loadPackageJson(packageJsonPath)) || undefined;
 }
 
-function loadPackageJsonFromDisk(file: string) {
+function loadJsonFromDisk(file: string) {
   const packageJson = require(file);
 
   return packageJson;
