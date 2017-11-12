056c8bd568c6a57cf3e81bb27a3df9812df096fe
jonathanmourtada <jonathan.mourtada@divid.se>
2017-11-12 21:41:02 +0100

    remove tsconfig dependency and fix tsconfig extends

--- src/tsconfig-loader.ts
+++ src/tsconfig-loader.ts
@@ -1,4 +1,6 @@
-import * as Tsconfig from "tsconfig";
+import * as path from "path";
+import * as fs from "fs";
+import * as R from "ramda";
 
 export interface TsConfigLoaderResult {
   tsConfigPath: string | undefined;
@@ -26,11 +28,42 @@ export function tsConfigLoader({
 
 function loadSyncDefault(cwd: string, filename?: string): TsConfigLoaderResult {
   // Tsconfig.loadSync uses path.resolve. This is why we can use an absolute path as filename
-  const loadResult = Tsconfig.loadSync(cwd, filename);
 
+  const configPath = resolveConfigPath(cwd, filename);
+  const config = loadConfig(configPath);
+  console.log(config);
   return {
-    tsConfigPath: loadResult.path,
-    baseUrl: loadResult.config.compilerOptions.baseUrl,
-    paths: loadResult.config.compilerOptions.paths
+    tsConfigPath: configPath,
+    baseUrl: config && config.compilerOptions && config.compilerOptions.baseUrl,
+    paths: config && config.compilerOptions && config.compilerOptions.paths
   };
+}
+
+function resolveConfigPath(cwd: string, filename?: string): string {
+  if (filename) {
+    const absolutePath = fs.lstatSync(filename).isDirectory()
+      ? path.resolve(filename, "./tsconfig.json")
+      : path.resolve(cwd, filename);
+
+    return absolutePath;
+  }
+
+  return path.resolve(cwd, "./tsconfig.json");
+}
+
+function loadConfig(
+  configFilePath: string
+): { [key: string]: any } | undefined {
+  if (!fs.existsSync(configFilePath)) {
+    return undefined;
+  }
+
+  const config = require(configFilePath);
+
+  if (config.extends) {
+    const currentDir = path.dirname(configFilePath);
+    const base = loadConfig(path.resolve(currentDir, config.extends)) || {};
+    return R.mergeDeepRight(base, config);
+  }
+  return config;
 }
