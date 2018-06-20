a83e0ddf0554559ea6443361b31c18ff667674e1
christoffer <christoffer.klang@gmail.com>
2018-06-20 13:32:04 +0200

    Re-introduce comment about unconditional return statement

--- src/match-path-async.ts
+++ src/match-path-async.ts
@@ -197,6 +197,14 @@ function findFirstExistingPath(
           }
         );
       }
+
+      // This is async code, we need to return unconditionally, otherwise the code still falls
+      // through and keeps recursing. While this might work in general, libraries that use neo-async
+      // like Webpack will actually not allow you to call the same callback twice.
+      //
+      // An example of where this caused issues:
+      // https://github.com/dividab/tsconfig-paths-webpack-plugin/issues/11
+      //
       // Continue with the next path
       return findFirstExistingPath(
         tryPaths,
