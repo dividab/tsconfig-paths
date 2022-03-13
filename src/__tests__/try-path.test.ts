02c37bc0b8b68402de2f4e7239236b065a119fe6
Bright Lee <benevbright@gmail.com>
2022-03-13 22:54:43 +0100

    Fix the root path (#180)
    * fix root path
    
    * add test

--- src/__tests__/try-path.test.ts
+++ src/__tests__/try-path.test.ts
@@ -10,6 +10,16 @@ describe("mapping-entry", () => {
     { pattern: "pre/fix/*", paths: [join("/absolute", "base", "url", "foo3")] },
     { pattern: "*", paths: [join("/absolute", "base", "url", "foo1")] },
   ];
+  const abosolutePathMappingsStarstWithSlash = [
+    {
+      pattern: "/opt/*",
+      paths: [join("/absolute", "src", "aws-layer")],
+    },
+    {
+      pattern: "*",
+      paths: [join("/absolute", "src")],
+    },
+  ];
   it("should return no paths for relative requested module", () => {
     const result = getPathsToTry(
       [".ts", "tsx"],
@@ -122,6 +132,50 @@ describe("mapping-entry", () => {
       {
         type: "index",
         path: join("/absolute", "base", "url", "foo1", "index.tsx"),
+      },
+    ]);
+  });
+
+  it("should resolve paths starting with a slash", () => {
+    const result = getPathsToTry(
+      [".ts"],
+      abosolutePathMappingsStarstWithSlash,
+      "/opt/utils"
+    );
+    expect(result).toEqual([
+      // "opt/*"
+      {
+        path: join("/absolute", "src", "aws-layer"),
+        type: "file",
+      },
+      {
+        path: join("/absolute", "src", "aws-layer.ts"),
+        type: "extension",
+      },
+      {
+        path: join("/absolute", "src", "aws-layer", "package.json"),
+        type: "package",
+      },
+      {
+        path: join("/absolute", "src", "aws-layer", "index.ts"),
+        type: "index",
+      },
+      // "*"
+      {
+        path: join("/absolute", "src"),
+        type: "file",
+      },
+      {
+        path: join("/absolute", "src.ts"),
+        type: "extension",
+      },
+      {
+        path: join("/absolute", "src", "package.json"),
+        type: "package",
+      },
+      {
+        path: join("/absolute", "src", "index.ts"),
+        type: "index",
       },
     ]);
   });
