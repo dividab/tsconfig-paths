7abdace1bd144ea747ed0fba647bd9b18114a344
Nathan Walters <nwalters@nerdwallet.com>
2022-05-02 11:51:48 -0700

    Fix failing test

--- src/__tests__/mapping-entry.test.ts
+++ src/__tests__/mapping-entry.test.ts
@@ -6,29 +6,12 @@ describe("mapping-entry", () => {
     const result = getAbsoluteMappingEntries(
       "/absolute/base/url",
       {
-        "*": ["/foo1", "/foo2"],
-        "longest/pre/fix/*": ["/foo2/bar"],
+        "*": ["/foo1", "./foo2"],
+        "longest/pre/fix/*": ["./foo2/bar"],
         "pre/fix/*": ["/foo3"],
       },
       true
     );
-    // assert.deepEqual(result, [
-    //   {
-    //     pattern: "longest/pre/fix/*",
-    //     paths: [join("/absolute", "base", "url", "foo2", "bar")],
-    //   },
-    //   {
-    //     pattern: "pre/fix/*",
-    //     paths: [join("/absolute", "base", "url", "foo3")],
-    //   },
-    //   {
-    //     pattern: "*",
-    //     paths: [
-    //       join("/absolute", "base", "url", "foo1"),
-    //       join("/absolute", "base", "url", "foo2"),
-    //     ],
-    //   },
-    // ]);
     expect(result).toEqual([
       {
         pattern: "longest/pre/fix/*",
@@ -36,26 +19,17 @@ describe("mapping-entry", () => {
       },
       {
         pattern: "pre/fix/*",
-        paths: [join("/absolute", "base", "url", "foo3")],
+        paths: [join("/foo3")],
       },
       {
         pattern: "*",
-        paths: [
-          join("/absolute", "base", "url", "foo1"),
-          join("/absolute", "base", "url", "foo2"),
-        ],
+        paths: [join("/foo1"), join("/absolute", "base", "url", "foo2")],
       },
     ]);
   });
 
   it("should should add a match-all pattern when requested", () => {
     let result = getAbsoluteMappingEntries("/absolute/base/url", {}, true);
-    // assert.deepEqual(result, [
-    //   {
-    //     pattern: "*",
-    //     paths: [join("/absolute", "base", "url", "*")],
-    //   },
-    // ]);
     expect(result).toEqual([
       {
         pattern: "*",
@@ -64,7 +38,6 @@ describe("mapping-entry", () => {
     ]);
 
     result = getAbsoluteMappingEntries("/absolute/base/url", {}, false);
-    // assert.deepEqual(result, []);
     expect(result).toEqual([]);
   });
 });
