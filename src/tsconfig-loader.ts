b935fe427b3fcc157243f0a6245ebded21849643
Ben Teichman <ben@voiceflow.com>
2023-03-29 15:19:46 -0400

    chore: remove unused functions and tests

--- src/tsconfig-loader.ts
+++ src/tsconfig-loader.ts
@@ -1,22 +1,6 @@
 import * as path from "path";
 import * as fs from "fs";
 import * as typescript from "typescript";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import JSON5 = require("json5");
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import StripBom = require("strip-bom");
-
-/**
- * Typing for the parts of tsconfig that we care about
- */
-export interface Tsconfig {
-  extends?: string;
-  compilerOptions?: {
-    baseUrl?: string;
-    paths?: { [key: string]: Array<string> };
-    strict?: boolean;
-  };
-}
 
 export interface TsConfigLoaderResult {
   tsConfigPath: string | undefined;
@@ -120,71 +104,4 @@ export function walkForTsConfig(
   }
 
   return walkForTsConfig(parentDirectory, readdirSync);
-}
-
-export function loadTsconfig(
-  configFilePath: string,
-  // eslint-disable-next-line no-shadow
-  existsSync: (path: string) => boolean = fs.existsSync,
-  readFileSync: (filename: string) => string = (filename: string) =>
-    fs.readFileSync(filename, "utf8")
-): Tsconfig | undefined {
-  if (!existsSync(configFilePath)) {
-    return undefined;
-  }
-
-  const configString = readFileSync(configFilePath);
-  const cleanedJson = StripBom(configString);
-  let config: Tsconfig;
-  try {
-    config = JSON5.parse(cleanedJson);
-  } catch (e) {
-    throw new Error(`${configFilePath} is malformed ${e.message}`);
-  }
-  let extendedConfig = config.extends;
-
-  if (extendedConfig) {
-    if (
-      typeof extendedConfig === "string" &&
-      extendedConfig.indexOf(".json") === -1
-    ) {
-      extendedConfig += ".json";
-    }
-    const currentDir = path.dirname(configFilePath);
-    let extendedConfigPath = path.join(currentDir, extendedConfig);
-    if (
-      extendedConfig.indexOf("/") !== -1 &&
-      extendedConfig.indexOf(".") !== -1 &&
-      !existsSync(extendedConfigPath)
-    ) {
-      extendedConfigPath = path.join(
-        currentDir,
-        "node_modules",
-        extendedConfig
-      );
-    }
-
-    const base =
-      loadTsconfig(extendedConfigPath, existsSync, readFileSync) || {};
-
-    // baseUrl should be interpreted as relative to the base tsconfig,
-    // but we need to update it so it is relative to the original tsconfig being loaded
-    if (base.compilerOptions && base.compilerOptions.baseUrl) {
-      const extendsDir = path.dirname(extendedConfig);
-      base.compilerOptions.baseUrl = path.join(
-        extendsDir,
-        base.compilerOptions.baseUrl
-      );
-    }
-
-    return {
-      ...base,
-      ...config,
-      compilerOptions: {
-        ...base.compilerOptions,
-        ...config.compilerOptions,
-      },
-    };
-  }
-  return config;
 }
