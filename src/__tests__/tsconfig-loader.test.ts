5156ef154aa0a223b426a5a916f070c7a8e0fa53
Bernardo <DanielSidhion@users.noreply.github.com>
2023-03-29 13:27:12 -0700

    Add support for extends as array of strings. (#245)
    * Add support for extends as array of strings.
    
    TypeScript 5.0 added support for defining "extends" as an array of
    strings. This commit adds support for this use case.
    
    It's important to note that even with this change, "baseUrl" and
    "paths" are still always being completely overwritten if a later
    tsconfig redefines any of those values.
    This might be confusing because a tsconfig may define
    "baseUrl=value1" and its own set of "paths" based on that baseUrl,
    but if a later tsconfig defines its own "baseUrl=value2", the
    overall config ends up becoming "baseUrl=value2" with the "paths"
    from the first config.
    
    This behaviour hasn't changed even when "extends" is an array of
    strings, so this commit maintains this behaviour.
    
    * Add test for array extends without .json extension.

--- src/__tests__/tsconfig-loader.test.ts
+++ src/__tests__/tsconfig-loader.test.ts
@@ -168,7 +168,7 @@ describe("walkForTsConfig", () => {
 });
 
 describe("loadConfig", () => {
-  it("It should load a config", () => {
+  it("should load a config", () => {
     const config = { compilerOptions: { baseUrl: "hej" } };
     const res = loadTsconfig(
       "/root/dir1/tsconfig.json",
@@ -178,7 +178,7 @@ describe("loadConfig", () => {
     expect(res).toStrictEqual(config);
   });
 
-  it("It should load a config with comments", () => {
+  it("should load a config with comments", () => {
     const config = { compilerOptions: { baseUrl: "hej" } };
     const res = loadTsconfig(
       "/root/dir1/tsconfig.json",
@@ -193,7 +193,7 @@ describe("loadConfig", () => {
     expect(res).toStrictEqual(config);
   });
 
-  it("It should load a config with trailing commas", () => {
+  it("should load a config with trailing commas", () => {
     const config = { compilerOptions: { baseUrl: "hej" } };
     const res = loadTsconfig(
       "/root/dir1/tsconfig.json",
@@ -207,7 +207,7 @@ describe("loadConfig", () => {
     expect(res).toStrictEqual(config);
   });
 
-  it("It should throw an error including the file path when encountering invalid JSON5", () => {
+  it("should throw an error including the file path when encountering invalid JSON5", () => {
     expect(() =>
       loadTsconfig(
         "/root/dir1/tsconfig.json",
@@ -221,7 +221,7 @@ describe("loadConfig", () => {
     );
   });
 
-  it("It should load a config with extends and overwrite all options", () => {
+  it("should load a config with string extends and overwrite all options", () => {
     const firstConfig = {
       extends: "../base-config.json",
       compilerOptions: { baseUrl: "kalle", paths: { foo: ["bar2"] } },
@@ -259,7 +259,7 @@ describe("loadConfig", () => {
     });
   });
 
-  it("It should load a config with extends from node_modules and overwrite all options", () => {
+  it("should load a config with string extends from node_modules and overwrite all options", () => {
     const firstConfig = {
       extends: "my-package/base-config.json",
       compilerOptions: { baseUrl: "kalle", paths: { foo: ["bar2"] } },
@@ -303,7 +303,7 @@ describe("loadConfig", () => {
     });
   });
 
-  it("Should use baseUrl relative to location of extended tsconfig", () => {
+  it("should use baseUrl relative to location of extended tsconfig", () => {
     const firstConfig = { compilerOptions: { baseUrl: "." } };
     const firstConfigPath = join("/root", "first-config.json");
     const secondConfig = { extends: "../first-config.json" };
@@ -333,6 +333,96 @@ describe("loadConfig", () => {
     expect(res).toEqual({
       extends: "../second-config.json",
       compilerOptions: { baseUrl: join("..", "..") },
+    });
+  });
+
+  it("should load a config with array extends and overwrite all options", () => {
+    const baseConfig1 = {
+      compilerOptions: { baseUrl: ".", paths: { foo: ["bar"] } },
+    };
+    const baseConfig1Path = join("/root", "base-config-1.json");
+    const baseConfig2 = { compilerOptions: { baseUrl: "." } };
+    const baseConfig2Path = join("/root", "dir1", "base-config-2.json");
+    const baseConfig3 = {
+      compilerOptions: { baseUrl: ".", paths: { foo: ["bar2"] } },
+    };
+    const baseConfig3Path = join("/root", "dir1", "dir2", "base-config-3.json");
+    const actualConfig = {
+      extends: [
+        "./base-config-1.json",
+        "./dir1/base-config-2.json",
+        "./dir1/dir2/base-config-3.json",
+      ],
+    };
+    const actualConfigPath = join("/root", "tsconfig.json");
+
+    const res = loadTsconfig(
+      join("/root", "tsconfig.json"),
+      (path) =>
+        [
+          baseConfig1Path,
+          baseConfig2Path,
+          baseConfig3Path,
+          actualConfigPath,
+        ].indexOf(path) >= 0,
+      (path) => {
+        if (path === baseConfig1Path) {
+          return JSON.stringify(baseConfig1);
+        }
+        if (path === baseConfig2Path) {
+          return JSON.stringify(baseConfig2);
+        }
+        if (path === baseConfig3Path) {
+          return JSON.stringify(baseConfig3);
+        }
+        if (path === actualConfigPath) {
+          return JSON.stringify(actualConfig);
+        }
+        return "";
+      }
+    );
+
+    expect(res).toEqual({
+      extends: [
+        "./base-config-1.json",
+        "./dir1/base-config-2.json",
+        "./dir1/dir2/base-config-3.json",
+      ],
+      compilerOptions: {
+        baseUrl: join("dir1", "dir2"),
+        paths: { foo: ["bar2"] },
+      },
+    });
+  });
+
+  it("should load a config with array extends without .json extension", () => {
+    const baseConfig = {
+      compilerOptions: { baseUrl: ".", paths: { foo: ["bar"] } },
+    };
+    const baseConfigPath = join("/root", "base-config-1.json");
+    const actualConfig = { extends: ["./base-config-1"] };
+    const actualConfigPath = join("/root", "tsconfig.json");
+
+    const res = loadTsconfig(
+      join("/root", "tsconfig.json"),
+      (path) => [baseConfigPath, actualConfigPath].indexOf(path) >= 0,
+      (path) => {
+        if (path === baseConfigPath) {
+          return JSON.stringify(baseConfig);
+        }
+        if (path === actualConfigPath) {
+          return JSON.stringify(actualConfig);
+        }
+        return "";
+      }
+    );
+
+    expect(res).toEqual({
+      extends: ["./base-config-1"],
+      compilerOptions: {
+        baseUrl: ".",
+        paths: { foo: ["bar"] },
+      },
     });
   });
 });
